2ab7f3e355ae65063f17a4f9d7dd10101407fc14
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2024-07-12 06:48:07 +0530

    Time: 0 ms (100%), Space: 12.3 MB (8.05%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,7 +1,26 @@
+int lower(int l, int h, vector<int> &nums, int target)
+{
+    int ans=h+1;
+    int mid;
+
+    while(l<=h){
+        mid=(l+h)/2;
+
+        if(nums[mid]>=target){
+            ans=mid;
+            h=mid-1;
+        }
+
+        else
+        l=mid+1;
+    }
+
+    return ans;
+}
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int ans=lower_bound(nums.begin(), nums.end(), target)-nums.begin();
+        int ans=lower(0, nums.size()-1, nums, target);
         return ans;
     }
 };
