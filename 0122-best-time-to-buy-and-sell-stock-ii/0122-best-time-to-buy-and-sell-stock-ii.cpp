5566725aa1a618e840b8090d882e352128fefd16
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2024-07-15 10:01:14 +0530

    Time: 10 ms (26.05%), Space: 18.1 MB (19.9%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,33 @@
+// int solve(int i, int buy, vector<int> &prices){
+//     if(i==prices.size())
+//     return 0;
+
+//     int take=0;
+//     int nottake=0;
+//     if(buy){
+//      take=-prices[i]+solve(i+1, 0, prices);
+//      nottake=solve(i+1, 1, prices);
+//     }
+
+//     else{
+//         take=prices[i]+solve(i+1, 1, prices);
+//         nottake=solve(i+1, 0, prices);
+//     }
+
+//     return max(take, nottake);
+// }
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int n=prices.size();
+        vector<vector<int>>  dp(n+1, vector<int> (2, 0));
+
+        for(int i=n-1;i>=0;i--){
+            dp[i][1]=max(-prices[i]+dp[i+1][0], dp[i+1][1]);
+            dp[i][0]=max(prices[i]+ dp[i+1][1], dp[i+1][0]);
+        }
+        return dp[0][1];
+    }
+};
