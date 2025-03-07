e6899280a8ae45822b25071014954448069858f9
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-03-07 20:10:44 +0530

    Time: 8 ms (13.61%), Space: 21.6 MB (9.08%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,47 @@
+void make_adj(int v, vector<vector<int>> &adj, vector<vector<int>> &isConnected){
+    for(int i=0;i<v;i++){
+        for(int j=0;j<v;j++){
+            if(isConnected[i][j]==1){
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+        }
+    }
+}
+
+void bfs(int i, vector<int> &visited, vector<vector<int>> &adj){
+    queue<int> q;
+    q.push(i);
+    visited[i]=1;
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+
+        for(auto it: adj[node]){
+            if(!visited[it]){
+                visited[it]=1;
+                q.push(it);
+            }
+        }
+    }
+}
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int v= isConnected.size();
+        vector<vector<int>> adj(v);
+        vector<int> visited(v, 0);
+        make_adj(v,adj, isConnected);
+int count=0;
+        for(int i=0;i<v;i++){
+            if(visited[i]==0){
+               count++;
+                bfs(i, visited, adj);
+
+            }
+        }
+
+        return count;
+    }
+};
