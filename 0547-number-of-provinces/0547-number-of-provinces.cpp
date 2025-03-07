aa7d0190d1e41882c430bd22ed7cdf2cfe866921
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-03-07 20:13:41 +0530

    Time: 8 ms (13.61%), Space: 21.1 MB (17.16%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -9,22 +9,32 @@ void make_adj(int v, vector<vector<int>> &adj, vector<vector<int>> &isConnected)
     }
 }
 
-void bfs(int i, vector<int> &visited, vector<vector<int>> &adj){
-    queue<int> q;
-    q.push(i);
-    visited[i]=1;
+// void bfs(int i, vector<int> &visited, vector<vector<int>> &adj){
+//     queue<int> q;
+//     q.push(i);
+//     visited[i]=1;
 
-    while(!q.empty()){
-        int node=q.front();
-        q.pop();
+//     while(!q.empty()){
+//         int node=q.front();
+//         q.pop();
 
-        for(auto it: adj[node]){
-            if(!visited[it]){
-                visited[it]=1;
-                q.push(it);
-            }
-        }
-    }
+//         for(auto it: adj[node]){
+//             if(!visited[it]){
+//                 visited[it]=1;
+//                 q.push(it);
+//             }
+//         }
+//     }
+// }
+
+
+void dfs(int i, vector<int> &visited, vector<vector<int>> &adj){
+if(visited[i])
+return;
+visited[i] =1;
+for(auto it: adj[i]){
+    dfs(it, visited, adj);
+}
 }
 class Solution {
 public:
@@ -37,8 +47,9 @@ int count=0;
         for(int i=0;i<v;i++){
             if(visited[i]==0){
                count++;
-                bfs(i, visited, adj);
+                // bfs(i, visited, adj);
 
+                dfs(i, visited, adj);
             }
         }
 
