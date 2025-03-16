cc5eecda29abb887fab284266a3fee837c7f83e0
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-03-16 11:44:09 +0530

    Time: 843 ms (15.92%), Space: 18.9 MB (96.61%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,63 @@
+int differ(string a, string b){
+    if(a.length()!=b.length()) return 0;
+int diffword=0;
+    for(int i=0;i<a.length();i++){
+        if(a[i]!=b[i])
+        diffword++;
+    }
+
+    return diffword;
+}
+
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int haveEndWord=0;
+        vector<int> visited(wordList.size(), 0);
+        for(auto it: wordList){
+            if(it==endWord){
+              haveEndWord=1;
+            }
+        }
+
+        if(haveEndWord==0) return 0;
+
+        int ans=1;
+        queue< pair<string, int>> q;
+        for(int i=0;i<wordList.size();i++){
+            if(wordList[i]==beginWord)
+            visited[i]=1;
+            
+        }
+q.push( {beginWord, 1});
+
+
+        while(!q.empty()){
+            auto [word, dist]= q.front();
+            if(word==endWord) return dist;
+            q.pop();
+
+            for(int i=0;i<wordList.size();i++){
+                if(visited[i]==0 && differ(word, wordList[i])==1){
+                    visited[i]=1;
+                    ans= dist+1;
+                    q.push({wordList[i], ans});
+                }
+            }
+
+
+        }
+
+
+       for(int i=0;i<wordList.size();i++){
+        if( wordList[i]==endWord && visited[i]==0){
+           return 0;
+        }
+       }
+
+       return ans;
+
+
+    }
+};
