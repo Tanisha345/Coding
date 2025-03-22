db177efff40cfb5c6ff68b18bd14cedcf020f9a9
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-03-22 17:17:20 +0530

    Time: 0 ms (100%), Space: 10.8 MB (21.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -14,6 +14,15 @@ public:
     int rob(vector<int>& nums) {
         int n= nums.size();
         vector<int> dp(n, -1);
-        return solve(n-1, nums, dp);
+        dp[0]=nums[0];
+        if(n>1)
+        dp[1]=max(nums[0], nums[1]);
+
+        for(int i=2;i<n;i++){
+            int take= nums[i]+dp[i-2];
+            int nottake=dp[i-1];
+            dp[i]= max(take, nottake);
+        }
+        return dp[n-1];
     }
 };
