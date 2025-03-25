92510d9d859d9bf3dc961d65028ebb47ae5e95b5
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-03-25 21:18:29 +0530

    Time: 105 ms (67.83%), Space: 100.2 MB (49.03%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -22,8 +22,26 @@ public:
         }
         if(sum%2!=0) return false;
         int target= sum/2;
-        vector<vector<int>> dp(n, vector<int> (target+1, -1));
-        return solve(n-1, nums, target, dp);
+        vector<vector<int>> dp(n, vector<int> (target+1, 0));
+
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+
+        if(nums[0]<=target){
+            dp[0][nums[0]]= 1;
+        }
+
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+                if(j>=nums[i])
+                dp[i][j]= dp[i-1][j-nums[i]] || dp[i-1][j];
+                else
+                dp[i][j]= dp[i-1][j];
+            }
+        }
+        return dp[n-1][target];
 
     }
 };
