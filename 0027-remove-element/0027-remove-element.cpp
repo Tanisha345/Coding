49e0903e5777c70fd5b9d5165954ad853d89a709
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-03-27 20:38:41 +0530

    Time: 0 ms (100%), Space: 11.8 MB (41.1%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int n=nums.size();
+        int n= nums.size();
         int i=0;
-        for(int j=0;j<n;j++){
-            if(nums[j]!=val){
-                nums[i]=nums[j];
+        int j= n-1;
+
+        while(i<=j){
+            if(nums[i]==val){
+                swap(nums[i], nums[j]);
+                j--;
+            }
+            else{
                 i++;
             }
         }
-
-        return i;
+        return j+1;
+        
     }
 };
