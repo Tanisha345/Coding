2520df5d9a87ac132f00d0fd2bd5cf3d5b41855c
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-04-14 19:57:05 +0530

    Time: 0 ms (100%), Space: 16.6 MB (24.49%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -10,40 +10,25 @@
  * };
  */
 
-void insertNode(TreeNode* &root, int val)
-{
-    if(!root){
-        root= new TreeNode(val);
-        return;
-    }
+TreeNode* solve(int left, int right, vector<int> & preorder){
 
-    TreeNode* temp= root;
-    while(temp){
-        if(val<temp->val)
-        {
-            if(temp->left) temp= temp->left;
-            else{
-                temp->left= new TreeNode(val);
-                return;
-            }
-        }else{
-            if(temp->right) temp=temp->right;
-            else{
-                temp->right= new TreeNode(val);
-                return;
-            }
-        }
+    if(left>right) return NULL;
 
-    }
-    return;
+    TreeNode* root= new TreeNode(preorder[left]);
+    int index=left+1;
+    while(index<=right && preorder[index]<preorder[left])
+    index++;
+    root->left= solve(left+1, index-1, preorder);
+    root->right= solve(index, right, preorder);
+    return root;
 }
 class Solution {
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        TreeNode* root=NULL;
-        for(int i=0;i<preorder.size();i++){
-            insertNode(root, preorder[i]);
-        }
-        return root;
+        if(preorder.size()==0) return NULL;
+        int left=0;
+        int right= preorder.size()-1;
+
+        return solve(left, right, preorder);
     }
 };
