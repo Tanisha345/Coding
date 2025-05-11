79352596f57b62af2f845bd6c003aa2b36ec2d58
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-05-11 17:48:10 +0530

    Time: 507 ms (5.21%), Space: 32.7 MB (40.12%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,18 +1,27 @@
+int func(vector<int>& nums, int goal){
+    int n=nums.size();
+    int i=0;
+    int sum=0;
+    int ans=0;
+
+    for(int j=0;j<n;j++){
+        sum+=nums[j];
+        while(i<=j && sum>goal){
+            sum-=nums[i];
+            i++;
+            cout<<i<<endl;
+        }
+
+        if(i<=j)
+        ans+=(j-i+1);
+    }
+    return ans;
+}
+
+
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int n= nums.size();
-        int ans=0;
-        int sum=0;
-        int i=0;
-        unordered_map<int, int> mp;
-      for(int j=0;j<n;j++){
-            sum+=nums[j];
-            if(sum==goal) ans++;
-            ans+=mp[sum-goal];
-            mp[sum]++;
-        }
-
-        return ans;
+        return func(nums, goal)-func(nums, goal-1);
     }
 };
