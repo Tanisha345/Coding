63810d20993daed66706c629fef178cfbcf60e3c
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-06-20 22:50:28 +0530

    Time: 0 ms (100%), Space: 11.1 MB (16.24%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,29 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+void solve(TreeNode* root, vector<int> &ans){
+    if(!root) return;
+
+    solve(root->left, ans);
+    solve(root->right, ans);
+    ans.push_back(root->val);
+}
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        if(!root) return ans;
 
-        stack<TreeNode*> st;
-        st.push(root);
-
-        while(!st.empty()){
-            TreeNode* node= st.top();
-            st.pop();
-            ans.push_back(node->val);
-
-            if(node->left){
-                st.push(node->left);
-            }
-            if(node->right){
-                st.push(node->right);
-            }
-        }
-
-        reverse(ans.begin(), ans.end());
+        solve(root, ans);
         return ans;
     }
 };
