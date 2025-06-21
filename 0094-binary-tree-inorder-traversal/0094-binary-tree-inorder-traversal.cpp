c0f6a53800db4095fdd7a850a8ca608681ff2442
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-06-21 18:16:22 +0530

    Time: 0 ms (100%), Space: 10.8 MB (64.52%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,20 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-void solve(TreeNode* root, vector<int> &ans){
-    if(!root) return;
-
-    solve(root->left, ans);
-    ans.push_back(root->val);
-    solve(root->right, ans);
-}
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        solve(root, ans);
-        return ans;
+        if(!root) return ans;
 
+        stack<TreeNode*> st;
+        TreeNode* temp=root;
+
+        while(true){
+            if(temp){
+                st.push(temp);
+                temp=temp->left;
+            }else{
+                if(st.empty()) break;
+                TreeNode* t= st.top();
+                st.pop();
+                ans.emplace_back(t->val);
+                temp=t->right;
+            }
+        }
+        return ans;
     }
 };
