4c4da606adae6375f7f8f1ffec5f42a56104649c
Tanisha aggarwal <120730408+Tanisha345@users.noreply.github.com>
2025-08-18 14:00:36 +0530

    Time: 0 ms (100%), Space: 18.5 MB (78.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,25 +3,53 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n= matrix.size();
         int m= matrix[0].size();
-        vector<int> rowFlag(n, 0);
-        vector<int> columnFlag(m, 0);
-       
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+       bool rowZero=false;
+       bool columnZero= false;
+
+       for (int j = 0; j < m; j++) {
+            if (matrix[0][j] == 0) {
+                rowZero = true;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                columnZero = true;
+                break;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
               if(matrix[i][j]==0){
-                rowFlag[i]=1;
-                columnFlag[j]=1;
+                matrix[i][0]=0;
+                matrix[0][j]=0;
               }
+           
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(rowFlag[i]==1 || columnFlag[j]==1){
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
+
             }
         }
+if (columnZero) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        // Handle first row
+        if (rowZero) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
         return;
     }
 };
